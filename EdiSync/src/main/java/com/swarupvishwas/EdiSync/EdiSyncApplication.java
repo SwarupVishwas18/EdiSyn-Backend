b75a1c1504c189d7414f3e64a341c8f162ce50a1
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-04-26 15:40:12 +0530

    Project Start

---
+++ EdiSync/src/main/java/com/swarupvishwas/EdiSync/EdiSyncApplication.java
@@ -0,0 +1,13 @@
+package com.swarupvishwas.EdiSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdiSyncApplication.class, args);
+	}
+
+}
---
+++ EdiSync/src/test/java/com/swarupvishwas/EdiSync/EdiSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swarupvishwas.EdiSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
